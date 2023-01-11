a5be35058bc63c62476584748a0add0ed201b46c
wochoa <woalvarado1@gmail.com>
2023-01-11 17:38:19 -0500

    v24

--- resources/js/components/paginas/autoridades.vue
+++ resources/js/components/paginas/autoridades.vue
@@ -40,6 +40,21 @@
                 </div>
             </div>
             <div class="col-md-6">
+                <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-dark">
+                    <div class="col p-4 d-flex flex-column position-static">
+                        <strong class="d-inline-block mb-2 text-warning">.....................</strong>
+                        <h3 class="mb-0 titulonot_2">.................</h3>
+
+                        <!-- <p class="card-text mb-auto titulonot_2">Vice Gobernador regional elegido mediante voluntad popular para el periodo 2019-2022</p> -->
+                        <p class="card-text mb-auto titulonot_2">Vice Gobernador Regional </p>
+
+                    </div>
+                    <div class="col-auto d-none d-lg-block">
+                        <img :src="ruta+'/img/gober2023/foto-incognito.jpg'" alt="" height="273">
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-md-6">
                 <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative bg-secondary">
                     <div class="col p-4 d-flex flex-column position-static">
                         <strong class="d-inline-block mb-2 text-warning">JUAN SIVORI PAREDES RIOS</strong>
@@ -53,7 +68,7 @@
                         <img :src="ruta+'/img/consejeros2023/01ptoinca.jpg'" alt="" height="273">
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
 
         <div class="row">
