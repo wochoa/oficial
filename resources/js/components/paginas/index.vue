ce561483c3b1007e9b5555ce7820bbf6cb1852fc
wochoa <woalvarado1@gmail.com>
2023-04-05 16:26:23 -0500

    v35

--- resources/js/components/paginas/index.vue
+++ resources/js/components/paginas/index.vue
@@ -123,7 +123,7 @@
 
                         <button type="button" data-bs-target="#carrusel_video" data-bs-slide-to="3" aria-label="Slide 4">
                         </button>
-                        <button type="button" data-bs-target="#carrusel_video" data-bs-slide-to="3" aria-label="Slide 4">
+                        <button type="button" data-bs-target="#carrusel_video" data-bs-slide-to="4" aria-label="Slide 5">
                         </button>
 
                     </ol>
